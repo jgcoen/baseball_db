841da1d7baa5126daafe935f3f28ad5cea3aef89
jgcoen <jgcoen30@gmail.com>
2021-03-23 21:06:13 -0400

    update the main script

--- scripts/main.py
+++ scripts/main.py
@@ -1,12 +1,29 @@
-import update_db
 import logging
+
+import pull_lahman
+import pull_retrosheet
+import pull_statcast
+import update_db
 from utils import configure_logging
 
-def main():
-    # logging.basicConfig(level=logging.DEBUG, format='%(asctime)s  %(levelname)s:%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-    logging.info('Do Something')
-    update_db.main()
 
+def main():
+
+    logging.info('Beginning to pull data from lahman')
+    pull_lahman.main()
+    logging.info('Finished pulling data from lahman')
+
+    logging.info('Beginning to pull data from retrosheet')
+    pull_retrosheet.main()
+    logging.info('Finished pulling data from retrosheet')
+
+    logging.info('Beginning to pull data from statcast')
+    pull_statcast.main()
+    logging.info('Finished pulling data from retrosheet')
+
+    logging.info('Beginning to update the database')
+    update_db.main()
+    logging.info('Finished updating the database')
 
 if __name__ == "__main__":
     configure_logging()
