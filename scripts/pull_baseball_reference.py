098c876d613b03bbd8d9e24e4218bf253a97104f
jgcoen <jgcoen30@gmail.com>
2021-07-02 09:25:48 -0400

    add schedule and record to pull br

--- scripts/pull_baseball_reference.py
+++ scripts/pull_baseball_reference.py
@@ -2,13 +2,13 @@ import logging
 
 from data_pull_classes import MultiYearDataPull, pull_single_table
 from pybaseball import (batting_stats_bref, bwar_bat, bwar_pitch,
-                        pitching_stats_bref, standings)
+                        pitching_stats_bref, standings, schedule_and_record)
 from utils import configure_logging
 
 
 def main():
 
-    logging.info('Beginning to pull standings')
+    logging.info('Beginning to pull baseball reference data')
     _standings = MultiYearDataPull(name='standings', schema ='baseball_reference', func=standings, min_year=1969, limit=50, add_year=True, current_year=True)
     _standings.update_table()
 
@@ -24,6 +24,8 @@ def main():
     _pitching_stats = MultiYearDataPull(name='pitching_stats_bref', schema ='baseball_reference', func=pitching_stats_bref, min_year=2008, limit=15, current_year=True, add_year=True)
     _pitching_stats.update_table()
 
+    _schedule_and_record = MultiYearDataPull(name='schedule_and_record', schema ='baseball_reference', func=schedule_and_record, min_year=1971, limit=10, current_year=True, add_year=True)
+    _schedule_and_record.update_table()
 
 if __name__ == "__main__":
     configure_logging()
